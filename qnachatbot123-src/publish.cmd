db4fe7e186227d4d36ecf073a4d1272820022435
sushanth123456 <92177003+sushanth123456@users.noreply.github.com>
2022-01-29 00:05:53 +0530

    Add files via upload
    My first commit of Chat Bot

---
+++ qnachatbot123-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\qnachatbot123-842a-Web-Deploy.pubxml -p:Password=AK04TRN8p0KYojmoHEoywzsYEfWSJ2GjerliCmLokZ4BbfaTrY6FT5zM21kS
+
